770de594bdf5eda778868481bb9dc3643687f437
Hiii-88888888 <monamyhi2008@gmail.com>
2025-08-31 13:20:35 +0600

    Update ex-commands.lua

--- plugin/ex-commands.lua
+++ plugin/ex-commands.lua
@@ -1,11 +1,11 @@
 vim.api.nvim_create_user_command(
-	"ScissorsAddNewSnippet",
-	function(args) require("scissors.1-prepare-selection").addNewSnippet(args) end,
+	"TweezersAddNewSnippet",
+	function(args) require("tweezers.1-prepare-selection").addNewSnippet(args) end,
 	{ desc = "Add new snippet.", range = true }
 )
 
 vim.api.nvim_create_user_command(
-	"ScissorsEditSnippet",
-	function() require("scissors.1-prepare-selection").editSnippet() end,
+	"TweezersEditSnippet",
+	function() require("tweezers.1-prepare-selection").editSnippet() end,
 	{ desc = "Edit existing snippet." }
 )
