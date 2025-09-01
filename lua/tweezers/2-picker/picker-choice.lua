13411457656528ef4c8017cb81c8d251fb2eda60
Hiii-88888888 <monamyhi2008@gmail.com>
2025-09-01 12:40:22 +0600

    Update and rename picker-choice.lua to picker-choice.lua

--- lua/tweezers/2-picker/picker-choice.lua
+++ lua/tweezers/2-picker/picker-choice.lua
@@ -1,20 +1,20 @@
 local M = {}
 --------------------------------------------------------------------------------
 
----@param allSnippets Scissors.SnippetObj[]
+---@param allSnippets Tweezers.SnippetObj[]
 function M.selectSnippet(allSnippets)
-	local icon = require("scissors.config").config.icons.scissors
+	local icon = require("tweezers.config").config.icons.tweezers
 	local prompt = vim.trim(icon .. " Select snippet: ")
-	local picker = require("scissors.config").config.snippetSelection.picker
+	local picker = require("tweezers.config").config.snippetSelection.picker
 	local hasTelescope, _ = pcall(require, "telescope")
 	local hasSnacks, _ = pcall(require, "snacks")
 
 	if picker == "telescope" or (picker == "auto" and hasTelescope) then
-		require("scissors.2-picker.telescope").selectSnippet(allSnippets, prompt)
+		require("tweezers.2-picker.telescope").selectSnippet(allSnippets, prompt)
 	elseif picker == "snacks" or (picker == "auto" and hasSnacks) then
-		require("scissors.2-picker.snacks").selectSnippet(allSnippets, prompt)
+		require("tweezers.2-picker.snacks").selectSnippet(allSnippets, prompt)
 	else
-		require("scissors.2-picker.vim-ui-select").selectSnippet(allSnippets, prompt)
+		require("tweezers.2-picker.vim-ui-select").selectSnippet(allSnippets, prompt)
 	end
 end
 
