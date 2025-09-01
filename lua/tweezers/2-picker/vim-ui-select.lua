ae33dd68500904bff63fb972bc37cb9ffa74f699
Hiii-88888888 <monamyhi2008@gmail.com>
2025-09-01 12:36:32 +0600

    Update and rename vim-ui-select.lua to vim-ui-select.lua

--- lua/tweezers/2-picker/vim-ui-select.lua
+++ lua/tweezers/2-picker/vim-ui-select.lua
@@ -1,16 +1,16 @@
 local M = {}
 
-local edit = require("scissors.3-edit-popup")
-local u = require("scissors.utils")
+local edit = require("tweezers.3-edit-popup")
+local u = require("tweezers.utils")
 --------------------------------------------------------------------------------
 
----@param snippets Scissors.SnippetObj[] entries
+---@param snippets Tweezers.SnippetObj[] entries
 ---@param prompt string
 function M.selectSnippet(snippets, prompt)
 	vim.ui.select(snippets, {
 		prompt = prompt,
 		format_item = function(snip)
-			local filename = vim.fs.basename(snip.fullPath):gsub("%.json$", "")
+			local filename = vim.fs.basename(snip.fullPath):gsub("%.lua$", "")
 			return u.snipDisplayName(snip) .. " [" .. filename .. "]"
 		end,
 	}, function(snip)
@@ -21,17 +21,17 @@ end
 
 --------------------------------------------------------------------------------
 
----@param allSnipFiles Scissors.snipFile[]
+---@param allSnipFiles Tweezers.snipFile[]
 ---@param bodyPrefill string[] for the new snippet
 function M.addSnippet(allSnipFiles, bodyPrefill)
-	local icon = require("scissors.config").config.icons.scissors
-	local snippetDir = require("scissors.config").config.snippetDir
+	local icon = require("tweezers.config").config.icons.tweezers
+	local snippetDir = require("tweezers.config").config.snippetDir
 
 	vim.ui.select(allSnipFiles, {
 		prompt = vim.trim(icon .. " Select file for new snippet: "),
 		format_item = function(item)
 			local relPath = item.path:sub(#snippetDir + 2)
-			return relPath:gsub("%.jsonc?$", "")
+			return relPath:gsub("%.luac?$", "")
 		end,
 	}, function(snipFile)
 		if not snipFile then return end
